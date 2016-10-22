0a878f2c75d24d102208a5c7c640365210060816
lophix <351962728@qq.com>
2016-10-22 15:06:42 +0800

    A test by my personal computer

--- src/com/flag/code/MessageSequence.java
+++ src/com/flag/code/MessageSequence.java
@@ -2,6 +2,6 @@ package com.flag.code;
 
 public class MessageSequence {
     public static void main(String[] args) {
-
+    	System.out.println("Its just a test for my personal computer");
     }
 }
