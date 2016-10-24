805c9cec27e7c17cb6c476bfe371f8615871af37
xujian <xujian_xtkf@win-sky.com.cn>
2016-10-24 11:24:54 +0800

    Build hadoop mapreduce test environment, add jars

--- src/com/flag/code/MessageSequence.java
+++ src/com/flag/code/MessageSequence.java
@@ -1,7 +1,27 @@
 package com.flag.code;
 
-public class MessageSequence {
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.Mapper;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reporter;
+
+public class MessageSequence extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
+	
+	private static final int MISSING = 9999;
+	
     public static void main(String[] args) {
     	System.out.println("Its just a test for my personal computer");
     }
+
+	@Override
+	public void map(LongWritable arg0, Text arg1, OutputCollector<Text, IntWritable> arg2, Reporter arg3)
+			throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
 }
