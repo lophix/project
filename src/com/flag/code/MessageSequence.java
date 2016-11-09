2b47869fbae1780336ee65884a2c76850741e164
xujian <xujian_xtkf@win-sky.com.cn>
2016-11-09 18:15:56 +0800

    some boring test

--- src/com/flag/code/MessageSequence.java
+++ src/com/flag/code/MessageSequence.java
@@ -4,5 +4,10 @@ public class MessageSequence {
 	
     public static void main(String[] args) {
     	System.out.println("Its just a test for my personal computer");
+        String str = "xmlxxx:xxdz:edit";
+        String[] strs = str.split(str);
+        for (String s : strs){
+
+        }
     }
 }
