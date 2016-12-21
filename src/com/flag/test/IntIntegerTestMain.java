b9684bf5c0d18b30aafc2a47b7a72a5c33258a1c
xujian <xujian_xtkf@win-sky.com.cn>
2016-12-21 11:35:52 +0800

    some boring test

--- src/com/flag/test/IntIntegerTestMain.java
+++ src/com/flag/test/IntIntegerTestMain.java
@@ -1,7 +1,6 @@
 package com.flag.test;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 /**
  * @Authuor Administrator
@@ -9,8 +8,18 @@ import java.util.List;
  */
 public class IntIntegerTestMain {
     public static void main(String[] args) {
-        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 11);
-        int a = 11;
-        System.out.println(list.contains(a));
+//        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 11);
+//        int a = 11;
+//        System.out.println(list.contains(a));
+        List<Map<String, String>> list = new ArrayList<>();
+        for (int i = 0 ; i < 10; i++){
+            Map<String, String> map = new HashMap<>();
+            map.put("hello", "hello" + i);
+            map.put("world", "world" + i);
+            list.add(map);
+        }
+        list.forEach((s) -> {
+            System.out.println(s.get("hello") + s.get("world"));
+        });
     }
 }
