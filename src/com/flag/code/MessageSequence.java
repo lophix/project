b45b398674b60824bb89beb6b7dc38e6a29c7d13
xujian <xujian_xtkf@win-sky.com.cn>
2017-01-12 15:16:35 +0800

    some boring test

--- src/com/flag/code/MessageSequence.java
+++ src/com/flag/code/MessageSequence.java
@@ -1,9 +1,14 @@
 package com.flag.code;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.util.Arrays;
 import java.util.List;
 
 public class MessageSequence {
+
+    private static Logger log = LogManager.getLogger(MessageSequence.class);
 	
     public static void main(String[] args) {
     	System.out.println("Its just a test for my personal computer");
@@ -12,5 +17,6 @@ public class MessageSequence {
         list.forEach(System.out::println);
 //        Float f = Float.valueOf(String.format("%.3f", null));
         System.out.println(String.format("%.3f", null));
+        log.info(" log test {}", str);
     }
 }
