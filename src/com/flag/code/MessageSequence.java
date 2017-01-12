3dd7be8d3c857ca45f73d926684d1816a9127363
xujian <xujian_xtkf@win-sky.com.cn>
2017-01-12 15:11:00 +0800

    some boring test

--- src/com/flag/code/MessageSequence.java
+++ src/com/flag/code/MessageSequence.java
@@ -10,5 +10,7 @@ public class MessageSequence {
         String str = "xmlxxx:xxdz:edit";
         List<String> list = Arrays.asList(str.split(":"));
         list.forEach(System.out::println);
+//        Float f = Float.valueOf(String.format("%.3f", null));
+        System.out.println(String.format("%.3f", null));
     }
 }
