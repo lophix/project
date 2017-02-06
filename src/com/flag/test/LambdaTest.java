fc508372b915a32a473ac7e35939ed127c3d2a64
xujian <xujian_xtkf@win-sky.com.cn>
2017-02-06 10:14:53 +0800

    some boring test

---
+++ src/com/flag/test/LambdaTest.java
@@ -0,0 +1,26 @@
+package com.flag.test;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * @Description
+ * @Author xuj
+ * @Create 2017-02-06-10:10
+ * @Version
+ */
+public class LambdaTest {
+    public static void main(String[] args) {
+        printIterator();
+    }
+
+    private static void printIterator(){
+        List<String> strings = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            strings.add(""+i);
+        }
+        Iterator it = strings.iterator();
+        it.forEachRemaining(System.out::println);
+    }
+}
