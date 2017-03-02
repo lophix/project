4afc30d53ccfbc879cac8153948d13d9026c8f88
xujian <xujian_xtkf@win-sky.com.cn>
2017-03-02 17:22:29 +0800

    some boring test

--- src/com/flag/util/BigEndianDecodeUtil.java
+++ src/com/flag/util/BigEndianDecodeUtil.java
@@ -40,7 +40,7 @@ public class BigEndianDecodeUtil {
 
     @Test
     public void test_byte2Int() {
-        byte[] bytes = new byte[]{0x01, 0x02, 0x03, 0x04};
+        byte[] bytes = new byte[]{0x00, 0x00, (byte) 0xff, (byte) 0xff};
         System.out.println(BigEndianDecodeUtil.bytes2Int(bytes, 0));
     }
 
@@ -54,5 +54,32 @@ public class BigEndianDecodeUtil {
     public void test_byte2BitStr() {
         byte b = 0x31;
         System.out.println(BigEndianDecodeUtil.byte2BitStr(b));
+    }
+
+    @Test
+    public void test_print(){
+        byte[] bytes = new byte[]{0x11, 0x02, 0x0c, 0x0b, 0x01, 0x02};
+        System.out.println(bytes2Int(bytes, 0));
+    }
+
+    public static String bcd2Str(byte[] bytes,int offset,int length) {
+
+        byte[] b = new byte[length];
+        //System.out.println(bytes.length+","+length);
+        System.arraycopy(bytes, offset, b, 0, length);
+
+
+        StringBuffer temp = new StringBuffer(b.length * 2);
+        for (int i = 0; i < b.length; i++) {
+            temp.append((byte) ((b[i] & 0xf0) >>> 4));
+            temp.append((byte) (b[i] & 0x0f));
+        }
+        String s = temp.toString();
+        return s;
+    }
+
+    @Test
+    public void test(){
+        System.out.println(0xff);
     }
 }
