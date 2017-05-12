c6b94458821ae5db8659e09b1a5ac31976687f7c
xujian <xujian_xtkf@win-sky.com.cn>
2017-05-12 15:37:35 +0800

    boring test

---
+++ project-test/src/main/java/com/flag/string/test/StringValueOfTest.java
@@ -0,0 +1,12 @@
+package com.flag.string.test;
+
+/**
+ * @author xuj
+ * @since 2017-05-12-15:27
+ */
+public class StringValueOfTest {
+    public static void main(String[] args) {
+        byte b = 0x01;
+        System.out.println(String.valueOf(b));
+    }
+}
