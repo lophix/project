5c50e996ed7e167d51c812ef3d5cca7a3159a4c1
xujian <xujian_xtkf@win-sky.com.cn>
2017-05-17 17:19:20 +0800

    boring test

--- project-test/src/main/java/com/flag/string/test/StringValueOfTest.java
+++ project-test/src/main/java/com/flag/string/test/StringValueOfTest.java
@@ -1,12 +1,38 @@
 package com.flag.string.test;
 
+import java.util.Scanner;
+
 /**
  * @author xuj
  * @since 2017-05-12-15:27
  */
 public class StringValueOfTest {
+//    public static void main(String[] args) {
+//        byte b = 0x01;
+//        System.out.println(String.valueOf(b));
+//    }
+
     public static void main(String[] args) {
-        byte b = 0x01;
-        System.out.println(String.valueOf(b));
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()) {
+            System.out.printf("%o\n",sc.nextInt());
+        }
+    }
+
+    public static int getResult(int n) {
+        int result = 0;
+        for(int i = 0; i < 32; i += 3) {
+            result += ((n >>> i) & 0x07) * (cal(i));
+        }
+        return result ;
+    }
+
+    private static int cal(int i) {
+        int m = i / 3;
+        int result = 1;
+        for (int j = 0; j < m; j++) {
+            result *= 10;
+        }
+        return result;
     }
 }
