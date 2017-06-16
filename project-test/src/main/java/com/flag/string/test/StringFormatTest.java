6b3298f61123e0eccd33b0376b02f1f57c502644
xujian <xujian_xtkf@win-sky.com.cn>
2017-06-16 15:23:45 +0800

    boring test

---
+++ project-test/src/main/java/com/flag/string/test/StringFormatTest.java
@@ -0,0 +1,11 @@
+package com.flag.string.test;
+
+/**
+ * @author xuj
+ * @since 2017-06-16-15:10
+ */
+public class StringFormatTest {
+    public static void main(String[] args) {
+        System.out.println(String.format("%.0f", 100.1001f));
+    }
+}
