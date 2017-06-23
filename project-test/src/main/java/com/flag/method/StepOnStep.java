30c361f5b77b2ca7da5a9f9f7a229cc5fa04fb0a
xujian <xujian_xtkf@win-sky.com.cn>
2017-06-23 10:40:29 +0800

    boring test

--- project-test/src/main/java/com/flag/method/StepOnStep.java
+++ project-test/src/main/java/com/flag/method/StepOnStep.java
@@ -1,8 +1,11 @@
 package com.flag.method;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import org.junit.Test;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 /**
  * N阶楼梯上楼问题：一次可以走两阶或一阶，问有多少种上楼方式。（要求采用非递归）
@@ -25,5 +28,39 @@ public class StepOnStep {
             System.out.println(n);
         }
         sc.close();
+    }
+
+    @Test
+    public void test(){
+        DateFormat dateFormat = new SimpleDateFormat("HHmmss");
+        try {
+            Date start = dateFormat.parse("113630");
+            Date current = new Date();
+            Date end = dateFormat.parse("125530");
+            System.out.println(isInRang(current, start, end));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private boolean isInRang(Date currDate , Date rangStart , Date rangeEnd){
+        Calendar currCal = convertDateToCalendar(currDate);
+        Calendar startCal = convertDateToCalendar(rangStart);
+        Calendar endRange = convertDateToCalendar(rangeEnd);
+
+        int currMint = currCal.get(Calendar.MINUTE);
+
+        int currHours = currCal.get(Calendar.HOUR_OF_DAY);
+
+        boolean startR = currHours > startCal.get(Calendar.HOUR_OF_DAY) || currHours == startCal.get(Calendar.HOUR_OF_DAY) && currMint >= startCal.get(Calendar.MINUTE);
+        boolean endR = currHours < endRange.get(Calendar.HOUR_OF_DAY) || currHours == endRange.get(Calendar.HOUR_OF_DAY) && currMint <= endRange.get(Calendar.MINUTE);
+        boolean b1 = startR&& endR;
+        return b1;
+    }
+
+    private Calendar convertDateToCalendar(Date data){
+        Calendar curr = Calendar.getInstance();
+        curr.setTime(data);
+        return curr;
     }
 }
