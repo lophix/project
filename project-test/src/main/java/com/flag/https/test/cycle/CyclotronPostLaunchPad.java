f0b5d6053a089e427d31f3ec9291e40ece897c13
xujian <xujian_xtkf@win-sky.com.cn>
2017-09-05 19:11:56 +0800

    boring test

--- project-test/src/main/java/com/flag/https/test/cycle/CyclotronPostLaunchPad.java
+++ project-test/src/main/java/com/flag/https/test/cycle/CyclotronPostLaunchPad.java
@@ -3,8 +3,10 @@ package com.flag.https.test.cycle;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 import org.apache.logging.log4j.LogManager;
@@ -16,9 +18,7 @@ import java.io.UnsupportedEncodingException;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
 import java.util.*;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 /**
  * @author xuj
@@ -27,47 +27,53 @@ import java.util.concurrent.TimeUnit;
 public class CyclotronPostLaunchPad {
 
     private static final ScheduledExecutorService SCHEDULED_EXECUTOR_SERVICE = Executors.newSingleThreadScheduledExecutor();
+    private static final ExecutorService EXECUTOR_SERVICE = Executors.newCachedThreadPool();
     private static final Logger LOGGER = LogManager.getLogger(CyclotronPostLaunchPad.class);
+    private static final int TREAD_NUM = 100;
+    private static final CountDownLatch COUNT_DOWN_LATCH = new CountDownLatch(TREAD_NUM);
 
     public static void main(String[] args) throws KeyManagementException, NoSuchAlgorithmException, UnsupportedEncodingException {
-        launching();
+        for (int i = 0; i < TREAD_NUM; i++) {
+            launching();
+            COUNT_DOWN_LATCH.countDown();
+        }
     }
 
     private static void launching() throws KeyManagementException, NoSuchAlgorithmException {
-        DefaultSSLClient httpClient = new DefaultSSLClient();
-        SCHEDULED_EXECUTOR_SERVICE.scheduleAtFixedRate(() -> {
+//        DefaultSSLClient httpClient = new DefaultSSLClient();
+        HttpClient httpClient = new DefaultHttpClient();
+        EXECUTOR_SERVICE.submit(() -> {
                     try {
-                        HttpResponse response = httpClient.execute(prepareRequest());
+                        HttpPost post = prepareRequest();
+                        COUNT_DOWN_LATCH.await();
+                        HttpResponse response = httpClient.execute(post);
                         if (response != null) {
                             HttpEntity resEntity = response.getEntity();
                             if (resEntity != null) {
                                 LOGGER.info("received message is {}", EntityUtils.toString(resEntity, "utf8"));
                             }
                         }
-                    } catch (IOException e) {
+                    } catch (IOException | InterruptedException e) {
                         e.printStackTrace();
                     }
-                },
-                0,
-                2,
-                TimeUnit.SECONDS);
+        });
     }
 
     private static HttpPost prepareRequest() throws IOException {
-        HttpPost request = new HttpPost("https://127.0.0.1:8443/timesharing");
+        HttpPost request = new HttpPost("http://192.168.10.114/VehicleManager/GetEVCInfoByRLID_V20?CusToken=3ebed107de964822bd93a7aead10c752&AppID=3ddb6f5a2cc1475b80a86ce4baac0a39&UserID=e374dc3e123a4e20864e0d83faeeeb10&RLID=168");
         request.addHeader("Accept", "*/*");
         request.addHeader("content-type", "application/x-www-form-urlencoded");
         request.addHeader("cache-control", "no-cache");
         request.addHeader("user-agent", "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36");
 
-        List<NameValuePair> nvps = new ArrayList<>();
-        nvps.add(new BasicNameValuePair("app_id", "201704282300007148"));
-        nvps.add(new BasicNameValuePair("method", "pt104"));
-        nvps.add(new BasicNameValuePair("charset", "utf-8"));
-        nvps.add(new BasicNameValuePair("version", "2017-07-03 03:07:50"));
-        nvps.add(new BasicNameValuePair("biz_content", bizContent()));
-        nvps.add(new BasicNameValuePair("sign", "ff8c067c71d7b620dbe832917ffc6f80"));
-        request.setEntity(new UrlEncodedFormEntity(nvps, "utf8"));
+//        List<NameValuePair> nvps = new ArrayList<>();
+//        nvps.add(new BasicNameValuePair("app_id", "201704282300007148"));
+//        nvps.add(new BasicNameValuePair("method", "pt104"));
+//        nvps.add(new BasicNameValuePair("charset", "utf-8"));
+//        nvps.add(new BasicNameValuePair("version", "2017-07-03 03:07:50"));
+//        nvps.add(new BasicNameValuePair("biz_content", bizContent()));
+//        nvps.add(new BasicNameValuePair("sign", "ff8c067c71d7b620dbe832917ffc6f80"));
+//        request.setEntity(new UrlEncodedFormEntity(nvps, "utf8"));
         return request;
     }
 
